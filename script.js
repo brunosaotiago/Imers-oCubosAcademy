fa319e7c2d4200e6a890ebfe98d1f2cf41ed5e60
Bruno Santiago <brunosantiago9988@gmail.com>
2022-05-11 22:28:50 -0300

    Sobe arquivos para o repositório

---
+++ script.js
@@ -0,0 +1,14 @@
+const nomeTimeA = prompt("Digite o nome do time A.");
+const nomeTimeB = prompt("Digite o nome do time B.");
+let pontoA = 0;
+let pontoB = 0;
+let setA = 0;
+let setB = 0;
+
+const nomeTimeA_HTML = document.getElementById("nomeTimeA")
+const nomeTimeB_HTML = document.getElementById('nomeTimeB');
+
+
+nomeTimeA_HTML.innerText = nomeTimeA;
+nomeTimeB_HTML.innerText = nomeTimeB;
+
